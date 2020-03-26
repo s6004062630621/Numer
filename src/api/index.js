4e970ae220fa19d6bf15ddf3574f7a354fb11f5c
s6004062630621 <54461786+s6004062630621@users.noreply.github.com>
2020-03-27 01:51:02 +0700

    Add files via upload

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://192.168.99.100:8080/api',
+    baseURL: 'http://192.168.99.101:8080/api',
 })
 
 export const insertMovie = payload => api.post(`/movie`, payload)
